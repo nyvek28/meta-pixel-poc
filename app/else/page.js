87493817523f25f7d337d8a8a2105d82d2fa627e
Kevyn Quirós <kevynquiros@gmail.com>
2024-07-15 12:44:56 -0600

    first

---
+++ app/else/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Else() {
+  return (
+    <main className="">
+      <h1>My Elsepage</h1>
+      <p>Lorem Ipsum Dolor</p>
+      <button>Click Me Again!</button>
+      <a href="/">Go home</a>
+    </main>
+  );
+}
